872ba8e44110be8f9e18f3d2da5158cf7a4ec18a
gness1804 <graham.nessler@gmail.com>
2016-11-09 22:19:19 -0700

    adding basic addItem functionality

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -16,15 +16,17 @@ class Application extends Component {
     //render ideas to page; pass the info into Output
   }
 
-  addNewItem(newItem){
-    console.log(newItem);
+  addNewItem(newItem) {
+    this.state.items.push(newItem);
+    
   }
 
   render() {
+
     return (
       <div>
         <h1 id="top-of-page">My Grocery List</h1>
-        <Input addNewItem={this.addNewItem} />
+        <Input addNewItem={this.addNewItem.bind(this)} />
         <Output />
         <a href="#top-of-page"><button id="top-of-page-button" type="button">Top of Page</button></a>
       </div>
