055d18549e095065c772dbbac84f7c664cf32876
gness1804 <graham.nessler@gmail.com>
2016-11-10 15:26:56 -0700

    working on edit function

--- lib/components/ItemCard.jsx
+++ lib/components/ItemCard.jsx
@@ -13,8 +13,12 @@ class ItemCard extends Component {
     this.props.deleteItem(id);
   }
 
-  editName(e){
+  makeNameEditable(e){
     this.setState({ nameEditable: true });
+  }
+
+  editName(e) {
+    
   }
 
   render() {
@@ -24,7 +28,7 @@ class ItemCard extends Component {
 
     return (
       <div id={id} className="each-idea-container">
-        <h2 contentEditable={ nameEditable } className="editable-item" onClick={(e) => { this.editName(e); }}>Item: {name}</h2>
+        <h2 contentEditable={ nameEditable } onChange={(e) => { this.editName(e) }} className="editable-item" onClick={(e) => { this.makeNameEditable(e); }}>Item: {name}</h2>
         <h3 contentEditable="false" className="editable-aisle">Aisle: {aisle}</h3>
         <h4 contentEditable="true" className="note">Your Note: {note}</h4>
         <h5 contentEditable="true" className="quantity">Quantity: {quantity}</h5>
