aec535f7cfc361e04d4a656d6947e5cb7841a249
gness1804 <graham.nessler@gmail.com>
2016-11-10 11:42:36 -0700

    successfully renders all items when in new component

--- lib/components/ItemCard.jsx
+++ lib/components/ItemCard.jsx
@@ -4,8 +4,17 @@ const ReactDOM = require('react-dom');
 class ItemCard extends Component {
 
   render() {
+
+    const { name, aisle, quantity, note, id } = this.props;
+
     return (
-      <div>Hello! This is a test.</div>
+      <div id={id} className="each-idea-container">
+        <h2 contentEditable="false" className="editable-item">Item: {name}</h2>
+        <h3 contentEditable="false" className="editable-aisle">Aisle: {aisle}</h3>
+        <h4 contentEditable="true" className="note">Your Note: {note}</h4>
+        <h5 contentEditable="true" className="quantity">Quantity: {quantity}</h5>
+        <button className="delete-button">Delete Item</button>
+      </div>
     );
   }
 
--- lib/components/Output.jsx
+++ lib/components/Output.jsx
@@ -4,22 +4,6 @@ const ReactDOM = require('react-dom');
 
 class Output extends Component {
 
-  deleteItem(){
-    console.log("test");
-  }
-
-  renderItems(item) {
-    return(
-      <div id={item.id} className="each-idea-container">
-        <h2 contentEditable="false" className="editable-item">Item: {item.name}</h2>
-        <h3 contentEditable="false" className="editable-aisle">Aisle: {item.aisle}</h3>
-        <h4 contentEditable="true" className="note">Your Note: {item.note}</h4>
-        <h5 contentEditable="true" className="quantity">Quantity: {item.quantity}</h5>
-        <button className="delete-button">Delete Item</button>
-      </div>
-    );
-  }
-
   render() {
 
     let itemList;
