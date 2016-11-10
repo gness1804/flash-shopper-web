78ced0cd6b37766835f7022cdf483d6d35267308
gness1804 <graham.nessler@gmail.com>
2016-11-09 21:05:38 -0700

    adds onChange functions to update state in Input

--- lib/components/Input.jsx
+++ lib/components/Input.jsx
@@ -20,8 +20,20 @@ class Input extends Component {
 
   }
 
+  updateQuantity(e){
+    this.setState({ quantity: e.target.value });
+  }
+
   updateName(e) {
     this.setState({ name: e.target.value });
+  }
+
+  updateNote(e) {
+    this.setState({ note: e.target.value });
+  }
+
+  updateAisle(e) {
+    this.setState({ aisle: e.target.value });
   }
 
   render() {
@@ -29,12 +41,12 @@ class Input extends Component {
 
     return (
       <div id="input-items-container">
-        <input id="item-input" type="text" placeholder="Item Name" list="groceries" onChange={() => { this.updateName(); }} />
+        <input id="item-input" type="text" placeholder="Item Name" list="groceries" onChange={(e) => { this.updateName(e); }} />
         <Datalist />
         <Category />
-        <input id="aisle-input" type="text" placeholder="Aisle" />
-        <input id="note" type="text" placeholder="Note" />
-        <input id="quantity" type="text" placeholder="Quantity (incl. unit)" />
+        <input id="aisle-input" type="text" placeholder="Aisle" onChange={(e) => { this.updateAisle(e); }} />
+        <input id="note" type="text" placeholder="Note" onChange={(e) => { this.updateNote(e) }} />
+        <input id="quantity" type="text" placeholder="Quantity (incl. unit)" onChange={(e) => { this.updateQuantity(e) }} />
         <button id="submit-button" type="button" onClick={() => { this.createNewItem(); }}>Submit</button>
         <button id="sort-items-button" type="button">Sort Items</button>
         <button id="delete-all-items-button" type="button" >Delete ALL Items!</button>
