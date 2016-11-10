6417e684a5334c9337f365afdd0ea551087722cd
gness1804 <graham.nessler@gmail.com>
2016-11-09 20:26:25 -0700

    built out basic page structure

---
+++ lib/components/Category.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+const ReactDOM = require('react-dom');
+
+class Category extends Component {
+
+  render() {
+    return (
+      <div>
+        <select id="category">
+               <option selected="selected">Please choose a category.</option>
+               <option>Bakery</option>
+               <option>Bottled Water</option>
+               <option>Breads/PBJ/Honey</option>
+               <option>Canned Beans/Tomatoes/Soups</option>
+               <option>Canned Fish/Ethnic Foods/Pasta+Pasta Sauce/Rice</option>
+               <option>Checkout</option>
+               <option>Chips/Nuts</option>
+               <option>Cooking Wines/Condiments/Olives</option>
+               <option>Deli/Prepared Foods</option>
+               <option>Dish+Laundry Detergent</option>
+               <option>Frozen Items</option>
+               <option>Household Goods</option>
+               <option>Meat</option>
+               <option>Medicines (OTC)</option>
+               <option>Office Supplies</option>
+               <option>Personal Care Items</option>
+               <option>Pest Control</option>
+               <option>Produce</option>
+               <option>Soda</option>
+               <option>Spices and Baking Items</option>
+         </select>
+      </div>
+    );
+  }
+
+}
+
+
+export default Category;
--- lib/components/Input.jsx
+++ lib/components/Input.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Datalist from './Datalist';
+import Category from './Category';
 
 const ReactDOM = require('react-dom');
 
@@ -6,7 +8,17 @@ class Input extends Component {
 
   render() {
     return (
-      <div>Hello! This is a test.</div>
+      <div id="input-items-container">
+        <input id="item-input" type="text" placeholder="Item" list="groceries" />
+        <Datalist />
+        <Category />
+        <input id="aisle-input" type="text" placeholder="Aisle" />
+        <input id="note" type="text" placeholder="Note" />
+        <input id="quantity" type="text" placeholder="Quantity (incl. unit)" />
+        <button id="submit-button" type="button">Submit</button>
+        <button id="sort-items-button" type="button">Sort Items</button>
+        <button id="delete-all-items-button" type="button" >Delete ALL Items!</button>
+        </div>
     );
   }
 
