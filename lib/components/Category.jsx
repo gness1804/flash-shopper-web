6c870e4d95838aadfda6b55f82322e0f70aff91f
gness1804 <graham.nessler@gmail.com>
2016-11-10 22:59:35 -0700

    fixes bugs with aisle assignment

--- lib/components/Category.jsx
+++ lib/components/Category.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+
 const ReactDOM = require('react-dom');
 
 class Category extends Component {
@@ -8,30 +9,23 @@ class Category extends Component {
     this.props.assignAisle(category);
   }
 
-
   render() {
     return (
       <div>
         <select id="category" defaultValue="Please choose a category." onChange={() => { this.assignAisle() }}>
                <option>Please choose a category.</option>
-               <option>Bakery</option>
                <option>Bottled Water</option>
                <option>Breads/PBJ/Honey</option>
                <option>Canned Beans/Tomatoes/Soups</option>
                <option>Canned Fish/Ethnic Foods/Pasta+Pasta Sauce/Rice</option>
-               <option>Checkout</option>
                <option>Chips/Nuts</option>
                <option>Cooking Wines/Condiments/Olives</option>
-               <option>Deli/Prepared Foods</option>
                <option>Dish+Laundry Detergent</option>
                <option>Frozen Items</option>
                <option>Household Goods</option>
-               <option>Meat</option>
                <option>Medicines (OTC)</option>
                <option>Office Supplies</option>
                <option>Personal Care Items</option>
-               <option>Pest Control</option>
-               <option>Produce</option>
                <option>Soda</option>
                <option>Spices and Baking Items</option>
          </select>
