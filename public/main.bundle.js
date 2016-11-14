6fccc0e2a8f6b3e1ac60ef743a6a2f615106884e
gness1804 <graham.nessler@gmail.com>
2016-11-14 10:21:15 -0700

    deployed to firebase

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -48100,7 +48100,7 @@
 	          ),
 	          _react2.default.createElement(
 	            'button',
-	            { disabled: true, className: 'edit-button', onClick: function onClick() {
+	            { hidden: true, className: 'edit-button', onClick: function onClick() {
 	                _this2.makeEditable();
 	              } },
 	            'Edit Item'
