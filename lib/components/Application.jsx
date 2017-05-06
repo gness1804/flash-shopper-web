5a65919202f77033019922d7698c6e2127e03a3f
gness1804 <graham.nessler@gmail.com>
2017-05-06 17:05:00 -0500

    Items now come in from firebase

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -4,18 +4,22 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import firebaseApp from '../../firebaseConfig';  // eslint-disable-line
 import AuthScreen from './AuthScreen'
+import cleanUpUserEmail from '../../helpers/cleanUpUserEmail'
 
 class Application extends Component {
   constructor() {
     super();
     this.state = {
+      items: [],
       showAuthScreen: true,
       userEmail: '',
       userId: '',
     }
+    this.itemsRef = {}
   }
 
   state: {
+    items: Array<Object>,
     showAuthScreen: boolean,
     userEmail: string,
     userId: string,
@@ -25,29 +29,48 @@ class Application extends Component {
     this.initializeApp()
   }
 
+  itemsRef: Object
+
+  hideAuthScreen = ():void => {
+    this.setState({ showAuthScreen: false })
+  }
+
   initializeApp = ():void => {
     firebase.auth().onAuthStateChanged((user: Object) => {
       if (user) {
-        console.log(user)
-        // const email = cleanUpUserEmail(user.email)
-        // this.setState({ userEmail: user.email })
-        // this.setState({ userId: user.uid })
-        // this.itemsRef = firebase.database().ref(email + '/main') //eslint-disable-line
-        // this.hideAuthScreen()
-        // this.listenForItems(this.itemsRef)
+        const email = cleanUpUserEmail(user.email)
+        this.setState({ userEmail: user.email })
+        this.setState({ userId: user.uid })
+        this.itemsRef = firebase.database().ref(email + '/main') //eslint-disable-line
+        this.hideAuthScreen()
+        this.listenForItems(this.itemsRef)
       }
     })
   }
 
-  render() {
-    const { showAuthScreen } = this.state
+  listenForItems = (itemsRef: Object):void => {
+    itemsRef.on('value', (snapshot: Array<{ name: string, aisle: string, note: string, quantity: string, inCart: boolean, key: string, val: Function}>) => {
+      const newArr = []
+      snapshot.forEach((item: { name: string, aisle: string, note: string, quantity: string, inCart: boolean, key: string, val: Function}) => {
+        newArr.push({
+          name: item.val().name,
+          aisle: item.val().aisle,
+          quantity: item.val().quantity,
+          note: item.val().note,
+          inCart: item.val().inCart || false,
+          id: item.key,
+        })
+      });
+      this.setState({ items: newArr })
+    });
+  }
 
+  render() {
+    const { showAuthScreen, items } = this.state
     return (
       <div>
         <h1 id="top-of-page">Flash Shopper</h1>
-        <AuthScreen
-          visible={showAuthScreen}
-        />
+        {showAuthScreen && <AuthScreen />}
       </div>
     );
   }
